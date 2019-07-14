ef54df83fa7e0aa22978ff9cb831ad563f971192
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-07-14 21:17:58 +0200

    add removal of class=active

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(event){
-      console.log('Link was clicked!');
-      console.log(event);
+    const titleClickHandler = function(event){
+        console.log('Link was clicked!');
+        console.log(event);
 
       /* remove class 'active' from all article links */
+        const activeLinks = document.querySelectorAll('.titles a.active');
+
+        for(let activeLink of activeLinks){
+            activeLink.classList.remove('active');
+        }
+
 
       /* add class 'active' to clicked link */
+        const activeArticles = document.querySelectorAll('.posts .post.active');
+
+        for(let activeArticle of activeArticles){
+            activeArticle.classList.remove('active');
+        }
 
       /* remove class 'active' from all articles */
 
